488241fc520a1021ea5cce0a15edaec62a426381
Gideon Oladipupo Shawana <135270937+gideonShawana@users.noreply.github.com>
2023-07-18 05:49:37 -0400

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectComponent } from './project/project.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '' ,
+    component: HomeComponent
+  },
+  {
+    path: 'About' ,
+    component: AboutComponent
+  },
+  {
+    path: 'project' ,
+    component: ProjectComponent
+  },
+  {
+    path: 'Service' ,
+    component: ServiceComponent
+  },
+  {
+    path: 'Contact' ,
+    component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,86 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { HomeComponent } from './home/home.component';
+// import { AboutComponent } from './about/about.component';
+// import { ProjectComponent } from './project/project.component';
+// import { ServiceComponent } from './service/service.component';
+// import { ContactComponent } from './contact/contact.component';
+
+// const routes: Routes = [
+//   {
+//     path: '' ,
+//     component: HomeComponent
+//   },
+//   {
+//     path: 'About' ,
+//     component: AboutComponent
+//   },
+//   {
+//     path: 'project' ,
+//     component: ProjectComponent
+//   },
+//   {
+//     path: 'Service' ,
+//     component: ServiceComponent
+//   },
+//   {
+//     path: 'Contact' ,
+//     component: ContactComponent
+//   }
+// ];
+
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectComponent } from './project/project.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectComponent,
+    ServiceComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      // { path: '', component: ProductListComponent },
+      // { path: 'products/:productId', component: ProductDetailsComponent },
+      // { path: 'cart', component: CartComponent },
+      // { path: 'shipping', component: ShippingComponent },
+
+      {
+        path: '' ,
+        component: HomeComponent
+      },
+      {
+        path: 'About' ,
+        component: AboutComponent
+      },
+      {
+        path: 'project' ,
+        component: ProjectComponent
+      },
+      {
+        path: 'Service' ,
+        component: ServiceComponent
+      },
+      {
+        path: 'Contact' ,
+        component: ContactComponent
+      }
+    ]),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
